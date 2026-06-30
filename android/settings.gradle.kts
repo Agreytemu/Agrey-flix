89e1694d874419f98fe79891ec745e4b4ed6b4dc
Agreytemu <agreytemu44@gmail.com>
2026-06-30 23:25:52 +0300

    Fix settings.gradle.kts include app module

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,17 +1,2 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "app"
+rootProject.name = "AgreyFlix"
 include(":app")
